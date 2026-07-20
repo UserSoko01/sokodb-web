f257cb190f8182d8c88030c4c2c6febe5d11ce2f
UserSoko01 <luis.flores@sokodb.com.mx>
2026-07-20 15:17:59 -0600

    Se actualiza la metadata global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,17 @@ import { Footer } from "@/components/layout/Footer";
 import { FloatingWhatsAppButton } from "@/components/shared/FloatingWhatsAppButton";
 
 const inter = Inter({
-  subsets:['latin'],
-  variable:'--font-sans'
+  subsets:["latin"],
+  variable:"--font-sans"
 });
 
 export const metadata: Metadata = {
   title: {
-    default: "Soko DB | Consultoría en bases de datos y desarrollo de software",
+    default: "Soko DB | Consultoría especializada en tecnologías de la información",
     template: "%s | Soko DB"
   },
   description: 
-  "Consultoría tecnológica especializada en bases de datos, desarrollo de software a la medida, analítica de datos y soluciones empresariales."
+  "Soluciones y consultoría especializada en plataformas de datos, servicios administrados, integración, automatización, analítica e infraestructura empresarial."
 };
 
 export default function RootLayout({
